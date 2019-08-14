3197139a8f01b7904c96e90db5b57b1cb1dab1c2
ggalfa <gleisoncosta705@gmail.com>
2019-08-14 02:33:54 -0300

    Verify tests

--- manager.py
+++ manager.py
@@ -3,7 +3,6 @@ from flask import Flask,jsonify
 app = Flask('Manager')
 
 tasks = []
-tasks.append({'id': 1, 'titulo': 'Python', 'descricao': 'Curso de Python e Flask'})
 
 @app.route('/tasks')
 def list():
