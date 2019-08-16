a0663aa9bd9bbbfcbd5bc2e73d5a4c31b664e266
ggalfa <gleisoncosta705@gmail.com>
2019-08-15 22:05:54 -0300

    Create test for def create task

--- test_task.py
+++ test_task.py
@@ -1,24 +1,26 @@
 from manager import app, tasks
+import json
 
-def test_list_tasks_return_status_200():
+
+def test_listed_tasks_return_status_200():
     app.config['TESTING'] = True
     with app.test_client() as cliente:
         response = cliente.get('/tasks')
         assert response.status_code == 200
 
-def test_list_tasks_return_format_jason():
+def test_listed_tasks_return_format_jason():
     app.config['TESTING'] = True
     with app.test_client() as cliente:
         response = cliente.get('/tasks')
         assert response.content_type == 'application/json'
 
-def test_list_of_task_empty_return_list_empty():
+def test_listed_of_task_empty_return_list_empty():
     app.config['TESTING'] = True
     with app.test_client() as cliente:
         response = cliente.get('/tasks')
         assert response.json == []
 
-def test_list_of_tasks_not_empty_return_content():
+def test_listed_of_tasks_not_empty_return_content():
     tasks.append({'id': 1, 'titulo': 'tarefa 1',
                    'descricao': 'tarefa numero 1', 'estado': False})
     app.config['TESTING'] = True
@@ -31,3 +33,51 @@ def test_list_of_tasks_not_empty_return_content():
             'estado': False
         }]
     tasks.clear()
+
+def test_create_task_accept_post():
+    with app.test_client() as cliente:
+        response = cliente.post('/task')
+        assert response.status_code != 405
+
+def test_create_task_return_task_inserted():
+    tasks.clear()
+    cliente = app.test_client()
+    # performs the request using the verb POST
+    response = cliente.post('/task', data=json.dumps({
+        'id': 'id',
+        'titulo': 'titulo',
+        'descricao': 'descricao'}),
+        content_type='application/json')
+    # analysis and transformation to python object of the answer is performed
+    data = json.loads(response.data.decode('utf-8'))
+    assert data['id'] == 1
+    # assert data['titulo'] == 'titulo'
+    # assert data['descricao'] == 'descricao'
+    # when the comparison is with True, False or None, is 'is'
+    assert data['estado'] is False
+
+def test_create_task_status_code_return_201():
+    with app.test_client() as cliente:
+        response = cliente.post('/task', data=json.dumps({
+            'titulo': 'titulo',
+            'descricao': 'descricao',}),
+            content_type='application/json')
+        assert response.status_code == 201
+
+def test_create_task_insert_element_in_blank():
+        tasks.clear()
+        with app.test_client() as cliente:
+            # make a request using the POST verb
+            cliente.post('/task', data=json.dumps({
+                'titulo': 'titulo',
+                'descricao': 'descricao'}),
+                content_type='application/json')
+            assert len(tasks) > 0
+
+# def test_crete_task_without_description():
+#     with app.test_client() as cliente:
+#         # status code must be 400 indicating a client error
+#         response = cliente.post('/task', data=json.dumps({'titulo': 'titulo'}),
+#                                 content_type='application/json')
+#         assert response.status_code == 400
+#     assert response.status_code == 201
