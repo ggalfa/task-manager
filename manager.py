26e09e3cd2463de8d5fdfeaba5039ced7fb94cbd
ggalfa <gleisoncosta705@gmail.com>
2019-08-15 22:05:08 -0300

    Create def create task

--- manager.py
+++ manager.py
@@ -1,9 +1,35 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
+import json
+
 
 app = Flask('Manager')
 
 tasks = []
+# tasks.append({
+#             'id': 1,
+#             'titulo': 'tarefa 1',
+#             'descricao': 'tarefa numero 1',
+#             'estado': False})
 
 @app.route('/tasks')
-def list():
+def listed():
     return jsonify(tasks)
+
+
+@app.route('/task', methods=['POST'])
+def create():
+    # titulo = request.json.get('titulo')
+    # descricao = request.json.get('descricao')
+    titulo = request.get_json('titulo')
+    descricao = request.get_json('descricao')
+    if not descricao or not titulo:
+        abort(400)
+        
+    task = {
+        'id': len(tasks) + 1,
+        'titulo': titulo,
+        'descricao': descricao,
+        'estado': False
+    }
+    tasks.append(task)
+    return jsonify(task), 201
